663f31c571b9759c508a5052252d861257beeec5
newphew92 <tko4@trusty>
2016-02-18 19:59:47 -0500

    final commit

--- src/bugger/src/bug.cpp
+++ src/bugger/src/bug.cpp
@@ -8,9 +8,7 @@
 #include <kobuki_msgs/MotorPower.h>
 #include <gazebo_msgs/ModelStates.h>
 #include <geometry_msgs/Pose.h>
-// #include <cmath>
 #include <math.h>
-#include <time.h>
 // ==================================Global vars===================================================
 const double PI = 3.1416;
 ros::Publisher motorPublisher;
@@ -21,40 +19,33 @@ geometry_msgs::Twist cmd;
 geometry_msgs::Pose robotPose;
 gazebo_msgs::ModelStates modeller;
 kobuki_msgs::MotorPower msgMotor;
-double goalX;
-double goalY;
+double goalX;double goalY;
 bool haveStart = false;
-double startX;
-double startY;
-double posX;
-double posY;
+double startX;double startY;
+double posX;double posY;
 double absAngle;
 double* eulerized;
-enum PlanningPhase { MLINE, WALL, CORNER };
+enum generalStates { MLINE, WALL, CORNER };
 char* phaseString[] = { "MLINE", "WALL", "CORNER" };
-enum CornerSubPhase { MOVE, TURN, ADJUST };
+enum turnStates { MOVE, TURN, ADJUST };
 char* CornerString[] = {"MOVE", "TURN", "ADJUST"};
-enum Direction { LEFT, MIDDLE, RIGHT, UNDEFINED };
-char* DirectionString[] = {"LEFT", "MIDDLE", "RIGHT", "UNDEFINED" };
+enum directions { LEFT, MIDDLE, RIGHT, UNDEFINED };
+char* directionString[] = {"LEFT", "MIDDLE", "RIGHT", "UNDEFINED" };
 
-int state;         // What planning phase the robot is in
-int cornerState;   // How far we are in our process of turning corners
-int numPoints;           // The number of points in the raw scan
-float scanInput[700];     // The raw readings by the scanner
-float simplifiedScan[3];   // Left, Right, Middle averages
-int scanIteration;       // Current repetition of the scan
-int sideWithWall;       // Tells use which side the wall we're following is on/should be
+int state;
+int cornerState;
+int numPoints;
+float scanInput[700];
+float simplifiedScan[3];
+int scanIteration;
+int sideWithWall;
 float wallDistBuffer;
-
-const float MOVEMENT_SPEED = 0.05;
-const float TURN_RATE  = 0.15;
-
-const float FOLLOW_DISTANCE = 1.5;
+const float MOVESPEED = 0.05;
+const float TURNSPEED  = 0.15;
+const float SAFEBOUND = 1.5;
 const float BUFFERSPACE = 1;
-
-
-const float CORNER_DROPOFF_DELTA = 1; // Used to detect how far of a difference in readings consists of a corner
-const float TURN_WAIT_TIME_SECONDS = 0.5;
+const float CORNERBUFFER = 0.75;
+const float TURNBUFFER = 0.5;
 // ===================================function declarations =======================================
 void getPos(const gazebo_msgs::ModelStates& modelStates);
 void forward(float dist);
@@ -67,21 +58,19 @@ void planPath(float destX, float destY, float orgX, float orgY, double goalAngle
 double toRad (double x);
 void wallRun();
 void turn (float rad);
-
 void processLaser(const sensor_msgs::LaserScan::ConstPtr& scan);
 void pathPlanner();
-void wallPhase();
-void mLinePhase();
-void cornerPhase();
 void turn (float rad);
 void forward(float dist);
 void turnAway(float dist);
+void wallStateManage();
+void mLineStateManage();
+void cornerStateManage();
 std::tr1::tuple<float, int> getNearestPointAndDirection();
 void smoothen();
 
 // ================================================================================================
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
   //initialize the node and name it
   ros::init(argc, argv, "bugAlg");
   ros::NodeHandle n;
@@ -99,9 +88,7 @@ int main(int argc, char **argv)
   sideWithWall = UNDEFINED;
   wallDistBuffer = 9999;
 
-  // ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("/scan",1000,processLaserScan);
-  // const sensor_msgs::LaserScan::ConstPtr& scan;
-  motorPublisher.publish(msgMotor); //enable engine!
+  motorPublisher.publish(msgMotor);
   ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1, processLaser);
   ros::spin();
 }
@@ -114,6 +101,7 @@ void getPos(const gazebo_msgs::ModelStates& modelStates){
   //save the starting point
   if (!haveStart){
     startX = posX; startY = posY; haveStart=true;
+    faceTarget(absAngle, toRad(eulerized[2]));
   }
   // planPath(goalX, goalY, posX, posY, absAngle, toRad(eulerized[2]));
   // printf("Position: %f, %f Start: %f, %f Goal: %f, %f\n", posX, posY, startX, startY, goalX, goalY);
@@ -122,73 +110,31 @@ void getPos(const gazebo_msgs::ModelStates& modelStates){
 }
 
 
-//###############################################################################
-
-void pathPlanner()
-{
-  // TODO: implement for phase 2 of bug algo under WALL case:
-  // IF robot is on MLINE
-    // IF robot is facing the goal AND there is a wall in front
-      // Follow the wall -- potential bug would be that this gets stuck within tolerances
-    // ELSE
-      // Change phase to MLINE phase
-        // The SLine phase will then turn towards the goal and either
-          // Follow the new wall right in front of it
-          // Move towards the goal
-  // ELSE
-    // Follow Wall
-  switch(state)
-  {
+void pathPlanner(){
+  switch(state){
     case MLINE:
-      mLinePhase();
+      mLineStateManage();
       break;
 
     case CORNER:
-      cornerPhase();
+      cornerStateManage();
       break;
 
     case WALL:
     default:
-      wallPhase();
+      wallStateManage();
       break;
   }
   wallDistBuffer = simplifiedScan[sideWithWall];
 }
-  // planPath(goalX, goalY, posX, posY, absAngle, toRad(eulerized[2]));
-// void planPath(float destX, float destY, float orgX, float orgY,double goalAngle, double orgAngle){
-//   if (isOnMLine(orgX, orgY)){
-//     printf("On M line\n");
-//     if (abs(goalAngle-orgAngle)<0.01){
-//       faceTarget(goalAngle, orgAngle);
-//       goToGoal(destX, destY, orgX, orgY);
-//       printf("going\n" );
-//     }
-//     else {
-//       // turn(0.5);
-//       printf("looking\n" );
-//       faceTarget(goalAngle, orgAngle);
-//     }
-//     // faceTarget(goalAngle, orgAngle);
-//     // temp check, change this
-//   //   if (!turning){
-//     // goToGoal(destX, destY, orgX, orgY);
-//   // }
-//   //    (closestPoint<=0.5) ? wallRun() :goToGoal(destX, destY, orgX, orgY);
-//   }
-//   // else{
-//     // wallRun();
-//     // forward(0.5);
-//   // }
-// }
 
-//#######################################################################################
-void mLinePhase(){
+void mLineStateManage(){
   std::tr1::tuple<float, int> point = getNearestPointAndDirection();
   float smallest = std::tr1::get<0>(point);
   int side = std::tr1::get<1>(point);
-  if(smallest > FOLLOW_DISTANCE || smallest == 0) // safe and not following wall
-  {
+  if(smallest > SAFEBOUND || smallest == 0) {
     faceTarget(absAngle, toRad(eulerized[2]));
+    ros::Duration(TURNBUFFER).sleep();
     if (isOnMLine(posX, posY)){
       printf("On M line\n");
       if (abs(absAngle-toRad(eulerized[2]))<0.01){
@@ -196,7 +142,6 @@ void mLinePhase(){
         printf("going\n" );
       }
       else {
-        // turn(0.5);
         printf("looking\n" );
         faceTarget(absAngle, toRad(eulerized[2]));
       }
@@ -207,8 +152,7 @@ void mLinePhase(){
       forward(1);
     }
   }
-  else if(smallest > BUFFERSPACE) // safe < smallest < follow, start following that wall
-  {
+  else if(smallest > BUFFERSPACE) {
     state = WALL;
 
     // Pick a side to put the wall and change the phase
@@ -229,24 +173,23 @@ void mLinePhase(){
     wallDistBuffer = simplifiedScan[sideWithWall];
   }
   else {
-    forward(-1 * MOVEMENT_SPEED);
+    forward(-1 * MOVESPEED);
   }
 }
-void cornerPhase(){
+void cornerStateManage(){
   if(cornerState == MOVE){
     float boost_into_doorway_speed = 1.10;
-    turnAway(TURN_RATE * 2);
-    ros::Duration(TURN_WAIT_TIME_SECONDS).sleep();
-    forward(boost_into_doorway_speed); // Get a good ways into the doorway
+    turnAway(TURNSPEED * 2);
+    ros::Duration(TURNBUFFER).sleep();
+    forward(boost_into_doorway_speed);
     cornerState = TURN;
   }
   else if(cornerState == TURN){
-    // Turn towards the wall until we can see the wall again and then move to adjust phase
-    if(simplifiedScan[sideWithWall] < FOLLOW_DISTANCE){
+    if(simplifiedScan[sideWithWall] < SAFEBOUND){
       cornerState = ADJUST;
     }
     else{
-      turnAway(-1 * TURN_RATE);
+      turnAway(-1 * TURNSPEED);
     }
   }
   else {
@@ -254,55 +197,43 @@ void cornerPhase(){
     cornerState = 0;
   }
 }
-void wallPhase()
-{
-  // Turn away if there is a wall in front
-  if(simplifiedScan[MIDDLE] < BUFFERSPACE)
-  {
-    turnAway(TURN_RATE * 2);
+void wallStateManage(){
+  if(simplifiedScan[MIDDLE] < BUFFERSPACE){
+    turnAway(TURNSPEED * 2);
   }
-  else // No wall in front of us
-  {
+  else{
     float wallDist = simplifiedScan[sideWithWall];
 
-    if( abs(wallDistBuffer - wallDist) > CORNER_DROPOFF_DELTA) // Dropoff was too big, we just saw a corner (or similar)
-    {
+    if( abs(wallDistBuffer - wallDist) > CORNERBUFFER){
       state = CORNER;
       cornerState = MOVE;
     }
-    // Turn away if the wall we're following is too close
-    else if(wallDist < BUFFERSPACE)
-    {
-      turnAway(TURN_RATE);
+    else if(wallDist < BUFFERSPACE){
+      turnAway(TURNSPEED);
     }
-    else if(wallDist > FOLLOW_DISTANCE) // Wall too far but not corner, turn towards it
-    {
-      turnAway(-1*TURN_RATE);
-      ros::Duration(TURN_WAIT_TIME_SECONDS).sleep();
-      forward(MOVEMENT_SPEED/2);
+    else if(wallDist > SAFEBOUND) {
+      turnAway(-1*TURNSPEED);
+      ros::Duration(TURNBUFFER).sleep();
+      forward(MOVESPEED/2);
     }
     // Distance is just right
-    else
-    {
-      forward(MOVEMENT_SPEED);
+    else{
+      forward(MOVESPEED);
     }
   }
 }
 void faceTarget(double goalAngle, double orgAngle){
   double angleToTurn = goalAngle-orgAngle;
-  int slice =1;
+  int slice =5;
     cmd.linear.x = 0;cmd.linear.y = 0;cmd.linear.z = 0;cmd.angular.x = 0;cmd.angular.y = 0;
     cmd.angular.z = (angleToTurn/slice);
-    printf("%f\n", goalAngle-orgAngle);
+    printf("Angle to turn %f\n", goalAngle-orgAngle);
     velocityPublisher.publish(cmd);
-    // angleToTurn-=(angleToTurn/slice);
 }
 void processLaser(const sensor_msgs::LaserScan::ConstPtr& scan){
-  const int scanRep = 5;
+  int scanRep = 6;
   numPoints =  (int)(scan->angle_max - scan->angle_min) / scan->angle_increment;
 
-  // Copy everything from the first scan into the local scan array
-  // After that only copy non non-NaN values.
   if(scanIteration < scanRep){
     for(int i = 0; i < numPoints; i++){
       if(scanIteration == 0){
@@ -314,50 +245,35 @@ void processLaser(const sensor_msgs::LaserScan::ConstPtr& scan){
     }
     scanIteration++;
   }
-  else // 5 scans complete
-  {
-    // Move
+  else  {
     smoothen();
     pathPlanner();
-
-    // Reset the scan counter
     scanIteration = 0;
   }
 }
 // Return <dist, side>
 std::tr1::tuple<float, int> getNearestPointAndDirection(){
-  float smallest = 9999;
-  int side = UNDEFINED;
-
-  if(simplifiedScan[0] < smallest && simplifiedScan[0] != 0){
-    simplifiedScan[0];
-  }
-
-  if(simplifiedScan[1] < smallest && simplifiedScan[1] != 0){
-    smallest = simplifiedScan[1];
-    side = MIDDLE;
-  }
-  if(simplifiedScan[2] < smallest && simplifiedScan[2] != 0){
-    smallest = simplifiedScan[2];
-    side = RIGHT;
-  }
+  float smallest = 99999;int side = UNDEFINED;
+  if(simplifiedScan[0] < smallest && simplifiedScan[0] != 0){simplifiedScan[0];}
+  if(simplifiedScan[1] < smallest && simplifiedScan[1] != 0){smallest = simplifiedScan[1];side = MIDDLE;}
+  if(simplifiedScan[2] < smallest && simplifiedScan[2] != 0){smallest = simplifiedScan[2];side = RIGHT;}
   return std::tr1::make_tuple(smallest, side);
 }
 
 float getClosestPoint(int start, int end){
-  float min = 9999;
+  float smallest = 99999;
   for(int i = start; i < end; i++){
-    if(scanInput[i] == scanInput[i] && min > scanInput[i]){
-      min = scanInput[i];
+    if(scanInput[i] == scanInput[i] && smallest > scanInput[i]){
+      smallest = scanInput[i];
     }
   }
-  return min;
+  return smallest;
 }
 void smoothen(){
-  const int rightIndexStop = 0;  // Which indices determine the vision for left, right and center parts of vision
-  const int rightIndexEnd = 150;
-  const int leftIndexStart = 450;
-  const int leftIndexEnd = 610;
+  int rightIndexStop = 0;
+  int rightIndexEnd = 150;
+  int leftIndexStart = 450;
+  int leftIndexEnd = 610;
   simplifiedScan[0] = getClosestPoint(leftIndexStart, leftIndexEnd);
   simplifiedScan[1] = getClosestPoint(rightIndexEnd, leftIndexStart);
   simplifiedScan[2] = getClosestPoint(rightIndexStop, rightIndexEnd);
@@ -365,8 +281,8 @@ void smoothen(){
 //go towards the goal. It's not relative to the bot but since it's only called when you're on the M line, it doesn't matter
 void goToGoal(float destX, float destY, float orgX, float orgY){
   cmd.linear.z = 0;cmd.angular.x = 0;cmd.angular.y = 0;cmd.angular.z = 0;
-  cmd.linear.y = (destY-orgY)/10;
-  cmd.linear.x = (destX-orgX)/10;
+  cmd.linear.y = (destY-orgY)/8;
+  cmd.linear.x = (destX-orgX)/8;
   velocityPublisher.publish(cmd);
 }
 // get target's angle from turtlebot based on world axis
@@ -397,16 +313,13 @@ void forward(float dist){
 }
 void turnAway(float rad){
   int turnModifier = 1;
-  if(sideWithWall == LEFT)
-  {
+  if(sideWithWall == LEFT){
     turnModifier = -1;
   }
   turn(turnModifier * rad);
 }
-void turn(float rad)
-{
-  cmd.linear.x = 0;cmd.linear.y = 0;cmd.linear.z = 0;cmd.angular.x = 0;cmd.angular.y = 0;
-  cmd.angular.z = rad;
+void turn(float rad){
+  cmd.linear.x = 0;cmd.linear.y = 0;cmd.linear.z = 0;cmd.angular.x = 0;cmd.angular.y = 0;cmd.angular.z = rad;
   velocityPublisher.publish(cmd);
 }
 double toRad (double x){
--- src/bugger/src/turtle.cpp
+++ src/bugger/src/turtle.cpp
@@ -1,20 +1,17 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-
 #include <tr1/tuple>
 #include <sstream>
 #include <sensor_msgs/LaserScan.h>
 #include <geometry_msgs/Twist.h>
 #include <kobuki_msgs/MotorPower.h>
 
-enum PlanningPhase { MLINE, WALL, CORNER };
-char* phaseString[] = { "MLINE", "WALL", "CORNER" }; // TODO: remove once debugging is complete
-enum CornerSubPhase { MOVE, TURN, ADJUST };
+enum generalStates { MLINE, WALL, CORNER };
+char* phaseString[] = { "MLINE", "WALL", "CORNER" };
+enum turnStates { MOVE, TURN, ADJUST };
 char* CornerString[] = {"MOVE", "TURN", "ADJUST"};
-enum Direction { LEFT, MIDDLE, RIGHT, NONE };
-char* DirectionString[] = {"LEFT", "MIDDLE", "RIGHT", "NONE" }; // TODO: remove once debugging is complete
-
-
+enum directions { LEFT, MIDDLE, RIGHT, UNDEFINED };
+char* directionString[] = {"LEFT", "MIDDLE", "RIGHT", "UNDEFINED" };
 int state;
 int cornerState;
 int numPoints;
@@ -30,33 +27,26 @@ ros::Publisher velocityPublisher;
 geometry_msgs::Twist cmd;
 kobuki_msgs::MotorPower Msg_motor;
 
-const float MOVEMENT_SPEED = 0.05;
-const float TURN_RATE  = 0.15;
-
-const float FOLLOW_DISTANCE = 1.5;
+const float MOVEMENTSPEED = 0.05;
+const float TURNSPEED  = 0.15;
+const float SAFEBOUND = 1.5;
 const float BUFFERSPACE = 1;
-
-
-const float CORNER_DROPOFF_DELTA = 0.75; // Used to detect how far of a difference in readings consists of a corner
-const float TURN_WAIT_TIME_SECONDS = 0.5;
-
+const float CORNERBUFFER = 0.75;
+const float PAUSETIME = 0.5;
 
 void processLaser(const sensor_msgs::LaserScan::ConstPtr& scan);
 void pathPlanner();
-void wallPhase();
-void mLinePhase();
-void cornerPhase();
 void turn (float rad);
 void forward(float dist);
 void turnAway(float dist);
+void wallStateManage();
+void mLineStateManage();
+void cornerStateManage();
 std::tr1::tuple<float, int> getNearestPointAndDirection();
 void smoothen();
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
   ros::init(argc, argv, "waller");
-
-  // Ros turn on overhead
   ros::NodeHandle n;
 	motorPublisher = n.advertise<kobuki_msgs::MotorPower>("/mobile_base/commands/motor_power", 1);
   velocityPublisher = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
@@ -65,7 +55,7 @@ int main(int argc, char **argv)
 
   // State initialization
   state = MLINE;
-  sideWithWall = NONE;
+  sideWithWall = UNDEFINED;
   wallDistBuffer = 9999;
 
   // All systems ready. BEGIN.
@@ -76,128 +66,75 @@ int main(int argc, char **argv)
 
 
 // Return <dist, side>
-std::tr1::tuple<float, int> getNearestPointAndDirection()
-{
-  float smallest = 9999;
-  int side = NONE;
-
-  if(simplifiedScan[0] < smallest && simplifiedScan[0] != 0)  {
-    simplifiedScan[0];
-  }
-
-  if(simplifiedScan[1] < smallest && simplifiedScan[1] != 0){
-    smallest = simplifiedScan[1];
-    side = MIDDLE;
-  }
-  if(simplifiedScan[2] < smallest && simplifiedScan[2] != 0){
-    smallest = simplifiedScan[2];
-    side = RIGHT;
-  }
+std::tr1::tuple<float, int> getNearestPointAndDirection(){
+  float smallest = 9999; int side = UNDEFINED;
+  if(simplifiedScan[0] < smallest && simplifiedScan[0] != 0){simplifiedScan[0];}
+  if(simplifiedScan[1] < smallest && simplifiedScan[1] != 0){smallest = simplifiedScan[1];side = MIDDLE;}
+  if(simplifiedScan[2] < smallest && simplifiedScan[2] != 0){smallest = simplifiedScan[2];side = RIGHT;}
   return std::tr1::make_tuple(smallest, side);
 }
 
-// Finds the closest point in the Raw Scan reading from start (inclusive) to end (exclusive).
-// Called by smoothen()
-float getClosestPoint(int start, int end)
-{
-  float min = 9999;
-  for(int i = start; i < end; i++)
-  {
-    if(scanInput[i] == scanInput[i] && min > scanInput[i])
-    {
-      min = scanInput[i];
+float getClosestPoint(int start, int end){
+  float smallest = 9999;
+  for(int i = start; i < end; i++){
+    if(scanInput[i] == scanInput[i] && smallest > scanInput[i]){
+      smallest = scanInput[i];
     }
   }
-  return min;
+  return smallest;
 }
 
-// Calculates the closest points of the left, right and center points
-// for smoothed vision (average of the left right and center points)
-
-
-// Scan the number of times specified by maxScanRep before moving. This is to prevent magical NaN misreads.
-// Then take the smallest non-NaN reading or use NaN if that was all that was read to plan the next move.
-void processLaser(const sensor_msgs::LaserScan::ConstPtr& scan)
-{
-  // Length of scan array
+void processLaser(const sensor_msgs::LaserScan::ConstPtr& scan){
+  int scanRep = 6;
   numPoints =  (int)(scan->angle_max - scan->angle_min) / scan->angle_increment;
-  const int scanRep = 5;
-  // Copy everything from the first scan into the local scan array
-  // After that only copy non non-NaN values.
-  if(scanIteration < scanRep)
-  {
-    for(int i = 0; i < numPoints; i++)
-    {
-      if(scanIteration == 0)
-      {
+
+  if(scanIteration < scanRep){
+    for(int i = 0; i < numPoints; i++){
+      if(scanIteration == 0){
         scanInput[i] = scan->ranges[i];
       }
-      else if(scanInput[i] == scanInput[i])
-      {
+      else if(scanInput[i] == scanInput[i]){
         scanInput[i] = scan->ranges[i];
       }
     }
     scanIteration++;
   }
-  else // 5 scans complete
-  {
-    // Move
+  else  {
     smoothen();
     pathPlanner();
-
-    // Reset the scan counter
     scanIteration = 0;
   }
 }
 
-void pathPlanner()
-{
-  // TODO: implement for phase 2 of bug algo under WALL case:
-  // IF robot is on MLINE
-    // IF robot is facing the goal AND there is a wall in front
-      // Follow the wall -- potential bug would be that this gets stuck within tolerances
-    // ELSE
-      // Change phase to MLINE phase
-        // The SLine phase will then turn towards the goal and either
-          // Follow the new wall right in front of it
-          // Move towards the goal
-  // ELSE
-    // Follow Wall
-  switch(state)
-  {
+void pathPlanner(){
+  switch(state){
     case MLINE:
-      mLinePhase();
+      mLineStateManage();
       break;
 
     case CORNER:
-      cornerPhase();
+      cornerStateManage();
       break;
 
     case WALL:
     default:
-      wallPhase();
+      wallStateManage();
       break;
   }
   wallDistBuffer = simplifiedScan[sideWithWall];
 }
 
-// In the wall runner code, this moves forward only until a wall comes within following distance.
-// In the full bug algorithm, it will follow the SLine.
-// Then it will check for a wall in front.
-void mLinePhase()
+void mLineStateManage()
 {
   std::tr1::tuple<float, int> point = getNearestPointAndDirection();
   float smallest = std::tr1::get<0>(point);
   int side = std::tr1::get<1>(point);
-  if(smallest > FOLLOW_DISTANCE || smallest == 0) // safe and not following wall
-  {
-    forward(MOVEMENT_SPEED);
+  if(smallest > SAFEBOUND || smallest == 0) {
+    forward(MOVEMENTSPEED);
+    //the proper function will be inserted here for the next part
   }
-  else if(smallest > BUFFERSPACE) // safe < smallest < follow, start following that wall
-  {
+  else if(smallest > BUFFERSPACE) {
     state = WALL;
-
-    // Pick a side to put the wall and change the phase
     if(side == MIDDLE) {
       if(simplifiedScan[LEFT] < simplifiedScan[RIGHT]){
         sideWithWall = LEFT;
@@ -215,61 +152,49 @@ void mLinePhase()
     wallDistBuffer = simplifiedScan[sideWithWall];
   }
   else {
-    forward(-1 * MOVEMENT_SPEED);
+    forward(-1 * MOVEMENTSPEED);
   }
 }
 
-// TODO
-void wallPhase()
-{
-  // Turn away if there is a wall in front
-  if(simplifiedScan[MIDDLE] < BUFFERSPACE)
-  {
-    turnAway(TURN_RATE * 2);
+void wallStateManage(){
+  if(simplifiedScan[MIDDLE] < BUFFERSPACE){
+    turnAway(TURNSPEED * 2);
   }
-  else // No wall in front of us
-  {
+  else {
     float wallDist = simplifiedScan[sideWithWall];
 
-    if( abs(wallDistBuffer - wallDist) > CORNER_DROPOFF_DELTA) // Dropoff was too big, we just saw a corner (or similar)
-    {
+    if( abs(wallDistBuffer - wallDist) > CORNERBUFFER){
       state = CORNER;
       cornerState = MOVE;
     }
-    // Turn away if the wall we're following is too close
-    else if(wallDist < BUFFERSPACE)
-    {
-      turnAway(TURN_RATE);
+    else if(wallDist < BUFFERSPACE){
+      turnAway(TURNSPEED);
     }
-    else if(wallDist > FOLLOW_DISTANCE) // Wall too far but not corner, turn towards it
-    {
-      turnAway(-1*TURN_RATE);
-      ros::Duration(TURN_WAIT_TIME_SECONDS).sleep();
-      forward(MOVEMENT_SPEED/2);
+    else if(wallDist > SAFEBOUND) {
+      turnAway(-1*TURNSPEED);
+      ros::Duration(PAUSETIME).sleep();
+      forward(MOVEMENTSPEED/2);
     }
-    // Distance is just right
-    else
-    {
-      forward(MOVEMENT_SPEED);
+    else{
+      forward(MOVEMENTSPEED);
     }
   }
 }
 
-void cornerPhase(){
+void cornerStateManage(){
   if(cornerState == MOVE){
     float boost_into_doorway_speed = 1.10;
-    turnAway(TURN_RATE * 2);
-    ros::Duration(TURN_WAIT_TIME_SECONDS).sleep();
-    forward(boost_into_doorway_speed); // Get a good ways into the doorway
+    turnAway(TURNSPEED * 2);
+    ros::Duration(PAUSETIME).sleep();
+    forward(boost_into_doorway_speed);
     cornerState = TURN;
   }
   else if(cornerState == TURN){
-    // Turn towards the wall until we can see the wall again and then move to adjust phase
-    if(simplifiedScan[sideWithWall] < FOLLOW_DISTANCE){
+    if(simplifiedScan[sideWithWall] < SAFEBOUND){
       cornerState = ADJUST;
     }
     else{
-      turnAway(-1 * TURN_RATE);
+      turnAway(-1 * TURNSPEED);
     }
   }
   else {
@@ -277,8 +202,7 @@ void cornerPhase(){
     cornerState = 0;
   }
 }
-void smoothen()
-{
+void smoothen(){
   const int rightIndexStop = 0;  // Which indices determine the vision for left, right and center parts of vision
   const int rightIndexEnd = 150;
   const int leftIndexStart = 450;
